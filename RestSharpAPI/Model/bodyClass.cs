54c651a7018c4675cce937953938fa9652cbb593
Pandzi0ch <jakub.struzik3@gmail.com>
2021-12-21 16:31:29 +0100

    Submit of the registration request
    Complete request of registration with writing the access token,
    filled Get all books order

--- RestSharpAPI/Model/bodyClass.cs
+++ RestSharpAPI/Model/bodyClass.cs
@@ -5,4 +5,10 @@
         public int bookId { get; set; }
         public string customerName { get; set; }
     }
+
+    internal class registerClass
+    {
+        public string clientName { get; set; }
+        public string clientEmail { get; set; }
+    }
 }
--- RestSharpAPI/fullSet.cs
+++ RestSharpAPI/fullSet.cs
@@ -3,9 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Newtonsoft.Json.Linq;
 using NUnit.Framework;
 using RestSharp;
 using RestSharp.Authenticators;
+using RestSharp.Serialization.Json;
 
 namespace RestSharpAPI
 {
@@ -17,8 +19,29 @@ namespace RestSharpAPI
         static Random random = new Random();
         public int randomNumber = random.Next(1, 6);
         public int postRandomNumber = random.Next(20, 99);
+        string username;
+        string email;
+        string token = "0d1441e87bc4cd9b9bb3f062336cc866c639d9e67ba83918ecf54c0ddba5a7d0";
+
+        [Test]
+        public void Registration()
+        {
+            username = "Example";
+            email = "exampleaddress@example.com";
+            var body = new registerClass { clientName = username, clientEmail = email };
 
 
+            var booksApi = new RestClient($"{url}");
+            var request = new RestRequest("/api-clients", DataFormat.Json);
+            request.AddJsonBody(body);
+            var response = booksApi.Post(request);
+
+            var deserialize = new JsonDeserializer();
+            var output = deserialize.Deserialize<Dictionary<string, string>>(response);
+            var accessToken = output["accessToken"];
+
+            Console.WriteLine(accessToken);
+        }
         [Test]
 
         public void GetBook()
@@ -27,7 +50,6 @@ namespace RestSharpAPI
             var booksApi = new RestClient($"{url}");
             var request = new RestRequest("status");
             var response = booksApi.Get(request);
-
 
         }
         [Test]
@@ -59,6 +81,7 @@ namespace RestSharpAPI
 
             var booksApi = new RestClient($"{url}");
             var request = new RestRequest("orders", DataFormat.Json);
+            request.AddHeader("Authorization", token);
             request.AddJsonBody(body);
             var response = booksApi.Post(request);
 
@@ -67,9 +90,19 @@ namespace RestSharpAPI
         public void GetAllOrder()
         {
             var booksApi = new RestClient($"{url}");
-            var request = new RestRequest("status", DataFormat.Json);
+            var request = new RestRequest("orders", DataFormat.Json);
             var response = booksApi.Get(request);
         }
+        [Test]
+        public void GetOneOrder()
+        {
+            var booksApi = new RestClient($"{url}");
+            var request = new RestRequest("books/{:bookId}")
+                .AddUrlSegment(":bookId", randomNumber);
+            request.AddHeader("Authorization", token);
+            var response = booksApi.Get(request);
+        }
+
         [Test]
         public void UpdateAnOrder()
         {
