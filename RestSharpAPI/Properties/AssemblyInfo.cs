5e5b4df39bd715887b1fcee46f8d62ebbd2ebcd1
Pandzi0ch <jakub.struzik3@gmail.com>
2021-12-21 12:32:18 +0100

    Adding basic GET and POST

---
+++ RestSharpAPI/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("RestSharpAPI")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("RestSharpAPI")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("91927867-47b7-4d78-8e8a-8962d5dab392")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ RestSharpAPI/UnitTest1.cs
@@ -0,0 +1,72 @@
+﻿using Newtonsoft.Json.Linq;
+using NUnit.Framework;
+using RestSharp;
+using RestSharp.Serialization.Json;
+using RestSharpAPI.Model;
+using System.Collections.Generic;
+
+namespace RestSharpAPI
+{
+    [TestFixture]
+    public class UnitTest1
+    {
+        [Test]
+        public void GetMethod()
+        {
+            var client = new RestClient("http://localhost:3000/");
+
+            var request = new RestRequest("posts/{postid}", Method.GET);
+            request.AddUrlSegment("postid", 1);
+
+            var response = client.Execute(request);
+
+            /*var deserialize = new JsonDeserializer();
+            var output = deserialize.Deserialize<Dictionary<string, string>>(response);
+            var result = output["author"];*/
+
+            JObject obs = JObject.Parse(response.Content);
+
+            Assert.That(obs["author"].ToString(), Is.EqualTo("typicode"), "Author is not correct");
+        }
+
+        [Test]
+        public void PostMethod()
+        {
+            var client = new RestClient("http://localhost:3000/");
+
+            var request = new RestRequest("posts/{postid}/profile", Method.POST);
+
+            request.RequestFormat = DataFormat.Json;
+            request.AddBody(new { name = "Raj" });
+
+            request.AddUrlSegment("postid", 1);
+
+            var response = client.Execute(request);
+
+            var deserialize = new JsonDeserializer();
+            var output = deserialize.Deserialize<Dictionary<string, string>>(response);
+            var result = output["name"];
+
+            Assert.That(result, Is.EqualTo("Raj"), "Author is not correct");
+        }
+
+        [Test]
+        public void PostWithTypeClass()
+        {
+            var client = new RestClient("http://localhost:3000/");
+
+            var request = new RestRequest("posts", Method.POST);
+
+            request.RequestFormat = DataFormat.Json;
+            request.AddBody(new Posts() { id = "22", author = "Jakub Struzik", title = "RestSharp" });
+
+            var response = client.Execute(request);
+
+            var deserialize = new JsonDeserializer();
+            var output = deserialize.Deserialize<Dictionary<string, string>>(response);
+            var result = output["author"];
+
+            Assert.That(result, Is.EqualTo("Jakub Struzik"), "Author is not correct");
+        }
+    }
+}
