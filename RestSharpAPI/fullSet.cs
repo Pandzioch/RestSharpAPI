72b2a5d231da76c6574015a047c6095cbf8d157f
Pandzi0ch <jakub.struzik3@gmail.com>
2021-12-21 14:29:51 +0100

    New template
    Creating a full template for Nunit + RestSharp for testing an API from Valentin Despa

---
+++ RestSharpAPI/fullSet.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using RestSharp;
+using RestSharp.Authenticators;
+
+namespace RestSharpAPI
+{
+    [TestFixture]
+    public class fullSet
+    {
+
+        public string url = "https://simple-books-api.glitch.me/";
+        
+
+        [Test]
+
+        public void GetBook()
+        {
+            
+            var booksApi = new RestClient($"{url}");
+            var request = new RestRequest("status", DataFormat.Json);
+            var response = booksApi.Get(request);
+        }
+/*        [Test]
+        public void GetListOfBooks()
+        {
+
+        }
+        [Test]
+        public void GetSingleBook()
+        {
+
+        }
+        [Test]
+        public void SubmitAnOrder()
+        {
+
+        }
+        [Test]
+        public void GetAllOrder()
+        {
+
+        }
+        [Test]
+        public void UpdateAnOrder()
+        {
+
+        }
+        [Test]
+        public void DeleteAnOrder()
+        {
+
+        }*/
+    }
+}
